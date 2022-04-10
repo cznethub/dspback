4bc779e06949393e2e2ac93b9778ffa6acfc28f7
Scott Black <sblack@cuahsi.org>
2022-04-10 12:21:31 -0700

    get metadata submission stubbed out

--- dspback/main.py
+++ dspback/main.py
@@ -6,7 +6,7 @@ from starlette.middleware.sessions import SessionMiddleware
 
 from dspback.config import get_settings
 from dspback.database.models import SessionLocal
-from dspback.routers import authentication, metadata_class, repository_authorization, submissions
+from dspback.routers import authentication, gitlab, metadata_class, repository_authorization, submissions
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=get_settings().session_secret_key)
@@ -18,6 +18,7 @@ app.include_router(
     repository_authorization.router, prefix="/api", tags=["Repository Authorization"], include_in_schema=False
 )
 app.include_router(metadata_class.router, prefix="/api")
+app.include_router(gitlab.router, prefix="/api")
 app.include_router(submissions.router, prefix="/api", tags=["Submissions"])
 
 
--- dspback/routers/gitlab.py
+++ dspback/routers/gitlab.py
@@ -1,3 +1,4 @@
+import base64
 import json
 
 import requests
@@ -21,7 +22,7 @@ class GitLabMetadataRoutes(MetadataRoutes):
     response_model = ResourceMetadata
     repository_type = RepositoryType.GITLAB
 
-    @router.post('/metadata/earthchem', tags=["EarthChem"])
+    @router.post('/metadata/gitlab', tags=["GitLab"])
     async def create_metadata_repository(self, metadata: request_model, identifier=None) -> response_model:
         if not identifier:
             response = requests.post(
@@ -39,7 +40,7 @@ class GitLabMetadataRoutes(MetadataRoutes):
             identifier = response.json()["id"]
 
         response = requests.post(
-            self.metadata_update_url,
+            self.update_url % identifier,
             data={"branch": "main", "content": metadata.json(indent=2), "commit_message": "some automated message"},
             params={
                 "access_token": self.access_token,
@@ -50,11 +51,11 @@ class GitLabMetadataRoutes(MetadataRoutes):
         if response.status_code >= 300:
             raise HTTPException(status_code=response.status_code, detail=response.text)
 
-        json_metadata = await self.get_metadata_repository(metadata, identifier)
+        json_metadata = await self.get_metadata_repository(identifier)
 
         return JSONResponse(json_metadata, status_code=201)
 
-    @router.put('/metadata/earthchem/{identifier}', tags=["EarthChem"])
+    @router.put('/metadata/gitlab/{identifier}', tags=["GitLab"])
     async def update_metadata(self, metadata: request_model_update, identifier) -> response_model:
         existing_metadata = await self.get_metadata_repository(identifier)
         incoming_metadata = metadata.json(skip_defaults=True, exclude_unset=True)
@@ -77,21 +78,24 @@ class GitLabMetadataRoutes(MetadataRoutes):
         return await self.get_metadata_repository(identifier)
 
     async def _retrieve_metadata_from_repository(self, identifier):
-        response = requests.get(self.read_url % identifier, params={"access_token": self.access_token})
+        def base64_decode(content):
+            return str(base64.b64decode(content), "utf-8")
+
+        response = requests.get(self.read_url % identifier + "?ref=main", params={"access_token": self.access_token})
 
         if response.status_code >= 300:
             raise HTTPException(status_code=response.status_code, detail=response.text)
 
-        json_metadata = json.loads(response.text)
+        json_metadata = json.loads(base64_decode(json.loads(response.text)["content"]))
         return json_metadata
 
-    @router.get('/metadata/earthchem/{identifier}', tags=["EarthChem"])
+    @router.get('/metadata/gitlab/{identifier}', tags=["GitLab"])
     async def get_metadata_repository(self, identifier) -> response_model:
         json_metadata = await self._retrieve_metadata_from_repository(identifier)
         await self.submit(identifier=identifier, json_metadata=json_metadata)
         return json_metadata
 
-    @router.delete('/metadata/earthchem/{identifier}', tags=["EarthChem"])
+    @router.delete('/metadata/gitlab/{identifier}', tags=["GitLab"])
     async def delete_metadata_repository(self, identifier):
         delete_submission(self.db, self.repository_type, identifier, self.user)
 
@@ -102,7 +106,7 @@ class GitLabMetadataRoutes(MetadataRoutes):
         if response.status_code >= 300:
             raise HTTPException(status_code=response.status_code, detail=response.text)
 
-    @router.put('/submit/earthchem/{identifier}', name="submit", tags=["EarthChem"])
+    @router.put('/submit/gitlab/{identifier}', name="submit", tags=["GitLab"])
     async def submit_repository_record(self, identifier: str):
         json_metadata = await self.submit(identifier)
         return json_metadata
