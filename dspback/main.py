41eb88efabe7b409df83748e6accb7f00d49dfca
Scott Black <sblack@cuahsi.org>
2023-03-16 15:47:49 -0600

    add local development mode with triggers/scheduler off

--- dspback/main.py
+++ dspback/main.py
@@ -4,6 +4,7 @@ import logging
 import uvicorn as uvicorn
 
 from dspback.api import app as app_fastapi
+from dspback.config import get_settings
 from dspback.scheduler import app as app_rocketry
 from dspback.triggers import watch_discovery_with_retry, watch_submissions_with_retry
 
@@ -17,13 +18,17 @@ class Server(uvicorn.Server):
 async def main():
     "Run Rocketry and FastAPI"
     server = Server(config=uvicorn.Config(app_fastapi, workers=1, loop="asyncio", host="0.0.0.0", port=5002))
+    settings = get_settings()
+    if settings.local_development:
+        api = asyncio.create_task(server.serve())
+        await asyncio.wait([api])
+    else:
+        api = asyncio.create_task(server.serve())
+        sched = asyncio.create_task(app_rocketry.serve())
+        discovery_trigger = asyncio.create_task(watch_discovery_with_retry())
+        submissions_trigger = asyncio.create_task(watch_submissions_with_retry())
 
-    api = asyncio.create_task(server.serve())
-    sched = asyncio.create_task(app_rocketry.serve())
-    discovery_trigger = asyncio.create_task(watch_discovery_with_retry())
-    submissions_trigger = asyncio.create_task(watch_submissions_with_retry())
-
-    await asyncio.wait([api, discovery_trigger, submissions_trigger, sched])
+        await asyncio.wait([api, discovery_trigger, submissions_trigger, sched])
 
 
 if __name__ == "__main__":
