9ae554af8c9defd15c98a2a840d43a9718095d5c
Scott Black <sblack@cuahsi.org>
2023-03-17 07:38:17 -0600

    check earthchem status for published indicator

--- dspback/routers/earthchem.py
+++ dspback/routers/earthchem.py
@@ -119,7 +119,7 @@ class EarthChemMetadataRoutes(MetadataRoutes):
                 json_metadata["leadAuthor"] = lead_author
                 json_metadata["contributors"] = all_contributors
 
-        return self.wrap_metadata(json_metadata, exists_and_is("datePublished", json_metadata))  # always published
+        return self.wrap_metadata(json_metadata, "status" in json_metadata and json_metadata["status"] == "complete")
 
     @router.get(
         '/metadata/earthchem/{identifier}',
