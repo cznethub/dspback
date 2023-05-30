a88f5c6f30fe36fea9fd1b19168ea8f364de13e1
Scott Black <sblack@cuahsi.org>
2023-05-30 09:23:11 -0600

    write out a table for the report management command

--- management/submissions_report.py
+++ management/submissions_report.py
@@ -28,11 +28,10 @@ async def main():
     submission_count_by_repository = {}
     test_submission_count_by_repository = {}
     submission_count_by_cluster = {}
-    funding_by_cluster = {}
-    submission_count_by_funding = {}
-    private_document_count = 0
     discoverable_documents_count = 0
+    table = [["id", "repository", "discoverable", "funding", "cluster"]]
     for submission in await Submission.all().to_list():
+        discoverable = False
         if "test" == submission.title.lower() or "asdf" in submission.title:
             submission_count = test_submission_count_by_repository.get(str(submission.repo_type), 0)
             test_submission_count_by_repository[str(submission.repo_type)] = submission_count + 1
@@ -42,44 +41,31 @@ async def main():
 
         discovery_document = await db["discovery"].find_one({"repository_identifier": submission.identifier})
         if discovery_document:
+            discoverable = True
             discoverable_documents_count = discoverable_documents_count + 1
             for cluster in discovery_document.get("clusters", []):
                 cluster_count = submission_count_by_cluster.get(cluster, 0)
                 submission_count_by_cluster[cluster] = cluster_count + 1
-
+            funding_identifiers = []
+            clusters = []
             if "funding" in discovery_document:
                 for funding in discovery_document["funding"]:
                     funding_identifier = funding.get("identifier", None)
-
-                    funding_id_match = None
-                    cluster_match = None
+                    funding_identifiers.append(funding_identifier)
                     for cluster_funding_id, cluster in cluster_by_id.items():
                         if cluster_funding_id in funding_identifier:
-                            funding_id_match= cluster_funding_id
-                            cluster_match = cluster
-                            break
-                    if funding_id_match:
-                        funding_ids = funding_by_cluster.get(cluster, set())
-                        funding_ids.update([funding_id_match])
-                        funding_by_cluster[cluster_match] = funding_ids
-                        submission_count = submission_count_by_funding.get(cluster_match, 0)
-                        submission_count_by_funding[cluster_match] = submission_count + 1
-        else:
-            private_document_count = private_document_count + 1
-    print("Submission Count By Repository (Discoverable and private)")
-    print(submission_count_by_repository)
-    print("\nTest submission count by repository")
-    print(test_submission_count_by_repository)
-    print("\nSubmission count by cluster (Discoverable Submissions)")
-    print(submission_count_by_cluster)
-    print("\nSubmission Funding identifiers by cluster (Discoverable Submissions)")
-    print(funding_by_cluster)
-    print("\nSubmission count by funding identifier (Discoverable Submissions)")
-    print(submission_count_by_funding)
-    print("\nNumber of documents not discoverable")
-    print(private_document_count)
-    print("\nNumber of documents discoverable")
-    print(discoverable_documents_count)
+                            clusters.append(cluster)
+        submission_row = [submission.identifier, submission.repo_type.name, discoverable, funding_identifiers, clusters]
+        table.append(submission_row)
+    import csv
+    with open('filename.csv', "w") as f:
+        filewriter = csv.writer(f)
+        for row in table:
+            filewriter.writerow(row)
+
+
+
+
 
 
 if __name__ == "__main__":
