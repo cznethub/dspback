530243d05813aa5d718372985ab277e40ed28d02
Scott Black <sblack@cuahsi.org>
2023-08-08 08:55:33 -0600

    [#183] - add the publication date and url to csv endpoint

--- dspback/routers/discovery.py
+++ dspback/routers/discovery.py
@@ -344,7 +344,7 @@ async def creator_search(request: Request, name: str, pageSize: int = 30) -> lis
 
 @router.get("/csv")
 async def csv(request: Request):
-    project = [{'$project': {'name': 1, 'description': 1, 'keywords': 1, '_id': 0}}]
+    project = [{'$project': {'name': 1, 'description': 1, 'keywords': 1, 'datePublished': 1, 'url': 1, '_id': 0}}]
     json_response = await request.app.db[get_settings().mongo_database]["discovery"].aggregate(project).to_list(None)
     df = pandas.read_json(json.dumps(json_response))
     filename = "file.csv"
