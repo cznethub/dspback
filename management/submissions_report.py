bfc1bcdde94d8509be3ac597d3c12651a9871890
Scott Black <sblack@cuahsi.org>
2023-09-20 15:23:41 -0600

    add submission date to the submissions report

--- management/submissions_report.py
+++ management/submissions_report.py
@@ -29,7 +29,7 @@ async def main():
     test_submission_count_by_repository = {}
     submission_count_by_cluster = {}
     discoverable_documents_count = 0
-    table = [["id", "repository", "discoverable", "funding", "cluster"]]
+    table = [["id", "repository", "discoverable", "funding", "cluster", "submission_date"]]
     for submission in await Submission.all().to_list():
         discoverable = False
         if "test" == submission.title.lower() or "asdf" in submission.title:
@@ -40,14 +40,14 @@ async def main():
             submission_count_by_repository[str(submission.repo_type)] = submission_count + 1
 
         discovery_document = await db["discovery"].find_one({"repository_identifier": submission.identifier})
+        funding_identifiers = []
+        clusters = []
         if discovery_document:
             discoverable = True
             discoverable_documents_count = discoverable_documents_count + 1
             for cluster in discovery_document.get("clusters", []):
                 cluster_count = submission_count_by_cluster.get(cluster, 0)
                 submission_count_by_cluster[cluster] = cluster_count + 1
-            funding_identifiers = []
-            clusters = []
             if "funding" in discovery_document:
                 for funding in discovery_document["funding"]:
                     funding_identifier = funding.get("identifier", None)
@@ -55,7 +55,7 @@ async def main():
                     for cluster_funding_id, cluster in cluster_by_id.items():
                         if cluster_funding_id in funding_identifier:
                             clusters.append(cluster)
-        submission_row = [submission.identifier, submission.repo_type.name, discoverable, funding_identifiers, clusters]
+        submission_row = [submission.identifier, submission.repo_type.name, discoverable, funding_identifiers, clusters, submission.submitted]
         table.append(submission_row)
     import csv
     with open('filename.csv', "w") as f:
