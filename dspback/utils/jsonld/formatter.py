78a3474c19297b977d2a2c5f9eb34b534d7143b4
Scott Black <sblack@cuahsi.org>
2024-01-11 19:44:17 -0700

    check for @list property befor accessing it

--- dspback/utils/jsonld/formatter.py
+++ dspback/utils/jsonld/formatter.py
@@ -57,13 +57,14 @@ def format_fields(json_ld):
             json_ld["license"] = {"text": json_ld["license"]}
 
     if "author" in json_ld:
-        author_roles = [author_list for author_list in json_ld['author']['@list']]
-        author_list = []
-        for author_role in author_roles:
-            if author_role:
-                author_list = author_list + author_role["author"]
+        if '@list' in json_ld['author']:
+            author_roles = [author_list for author_list in json_ld['author']['@list']]
+            author_list = []
+            for author_role in author_roles:
+                if author_role:
+                    author_list = author_list + author_role["author"]
 
-        json_ld["creator"] = {'@list': author_list}
+            json_ld["creator"] = {'@list': author_list}
 
     if "@context" in json_ld:
         if not isinstance(json_ld["@context"], str):
