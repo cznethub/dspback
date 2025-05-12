eecf3b7affb59ee991c33b0c25af6d068c2721f6
Maurier Ramirez <mauriel.ramirez@gmail.com>
2025-05-12 10:15:22 -0600

    fix: add missing function parameter (#243)

--- dspback/routers/metadata_class.py
+++ dspback/routers/metadata_class.py
@@ -32,9 +32,9 @@ class MetadataRoutes:
         repository_token = await get_current_repository_token(request, self.repository_type, self.user, self.settings)
         return repository_token.access_token
 
-    def wrap_metadata(self, metadata: dict, published: bool, is_public):
+    def wrap_metadata(self, metadata: dict, is_published: bool, is_public):
         # TODO: add another flag `can_edit` indicating if the user has edit permit on this resource
-        return {"metadata": metadata, "published": published, "public": is_public or False}
+        return {"metadata": metadata, "published": is_published, "public": is_public or False}
 
     def __init__(self):
         if self.request_model is None:
