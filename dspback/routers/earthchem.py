fda2fd835a1d78205661799083e22cc9bf20238e
Maurier <mauriel.ramirez@gmail.com>
2026-04-14 14:12:58 -0600

    fix: earthchem public view url

--- dspback/routers/earthchem.py
+++ dspback/routers/earthchem.py
@@ -95,17 +95,8 @@ class EarthChemMetadataRoutes(MetadataRoutes):
 
     async def _retrieve_metadata_from_repository(self, request: Request, identifier):
         # Try to fetch the resource. Published/public resources can be accessed without an access token.
-        response = requests.get(self.read_url % identifier)
-        access_token = None
+        response = requests.get(self.public_view_url % identifier)
         if response.status_code >= 300:
-            # If permission is required, try using the access token
-            access_token = await self.access_token(request)
-            response = requests.get(
-                self.read_url % identifier,
-                headers={"accept": "application/json", "Authorization": "Bearer " + str(access_token)},
-            )
-
-            if response.status_code >= 300:
                 raise RepositoryException(status_code=response.status_code, detail=response.text)
 
         # split first contributors to leadAuthor
